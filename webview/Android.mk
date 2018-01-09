e90f18328c68b6ebffeb074b2c7f5e3d0a0ca1d3
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-09 14:32:49 +0530

    Delete Android.mk

--- webview/Android.mk
+++
@@ -1,8 +0,0 @@
-ZPATH := vendor/fly/IQ4511/webview
-
-$(shell cp $(ZPATH)/arm/libwebviewchromium.so              $(TARGET_OUT)/lib)
-$(shell cp $(ZPATH)/arm/libwebviewchromium_loader.so       $(TARGET_OUT)/lib)
-$(shell cp $(ZPATH)/arm/libwebviewchromium_plat_support.so $(TARGET_OUT)/lib)
-
-include $(ZPATH)/webview/Android.mk
-include $(ZPATH)/postbuild/Android.mk
