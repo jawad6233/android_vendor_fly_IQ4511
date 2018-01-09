54f04a2c41f2427a1d72da47db1ff71620c45043
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-09 15:34:25 +0530

    Add files via upload

---
+++ IrisX8-vendor.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/system/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/system/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/system/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/system/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/system/xbin,system/xbin)
+
